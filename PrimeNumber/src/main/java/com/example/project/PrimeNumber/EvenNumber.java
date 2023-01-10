29cb20d2ee5964c0b45da318ba70b2cc523f304a
nivetha-ts <tsnivethabtechit@gmail.com>
2023-01-10 14:34:09 +0000

    first-commit

---
+++ PrimeNumber/src/main/java/com/example/project/PrimeNumber/EvenNumber.java
@@ -0,0 +1,14 @@
+package com.example.project.PrimeNumber;
+
+public class EvenNumber {
+	
+	public static void main(String args[]) {
+		int n1=6;
+		if(n1%2==0) {
+			System.out.println("Even Number");
+		}else {
+			System.out.println("Odd Number");
+		}
+	}
+
+}
---
+++ PrimeNumber/src/main/java/com/example/project/PrimeNumber/PrimeNumberApplication.java
@@ -0,0 +1,13 @@
+package com.example.project.PrimeNumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumberApplication.class, args);
+	}
+
+}
